390e1342073a5194a1a8687022e5943f140e4e73
davidwanlongwu <2490050603@qq.com>
2025-03-08 23:24:37 -0700

    responsive style for home page and login register form

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -3,15 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div
-        style={{
-          position: "absolute",
-          top: "60px",
-          right: "120px",
-          display: "flex",
-          gap: "20px",
-        }}
-      >
+      <div className="header_buttons">
         <Link href="pages/login">
           <button className="login_btn">Log In</button>
         </Link>
@@ -20,6 +12,7 @@ export default function Home() {
         </Link>
       </div>
       <p className="welcome_text">Welcome to our blog world! </p>
+      <img src="/pet.jpg" alt="pet" className="pet_image" />
     </>
   );
 }
