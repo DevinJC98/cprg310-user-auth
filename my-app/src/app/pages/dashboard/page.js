eb6f18b8f4e2258de37845802cfc9f7e70035006
DevinJC98 <devinjc98@hotmail.com>
2025-03-08 17:41:12 -0700

    Update dashboard to display user email
    gave authenticated uses more permissions that allow the client to read their first name, last name, and email

--- my-app/src/app/pages/dashboard/page.js
+++ my-app/src/app/pages/dashboard/page.js
@@ -1,16 +1,16 @@
 import { getUserData } from "../../../../lib/dal";
 import client from "../../../../lib/directus";
-import { readItems } from "@directus/sdk";
+import { readItems, readMe } from "@directus/sdk";
 import "./dashboard.css";
 
 export default async function Dashboard() {
-  const response = await getUserData();
-
+  const userdata = await client.request(readMe({ fields: ["*"] }));
   const posts = await client.request(readItems("Posts"));
 
   return (
     <main>
-      <h1>Hello {response?.user?.email}</h1>
+      <button onClick={console.log(userdata)}>console.log</button>
+      <h1>Hello {userdata.first_name + " " + userdata.last_name}</h1>
       <form action="/api/auth/logout" method="POST">
         <button type="submit"> Log Out </button>
       </form>
